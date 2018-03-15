4e0cbc5aa3acb6f71418c59d420045092a6dd7c6
Collin McKinney <collinmmckinney@gmail.com>
2018-03-15 18:03:33 -0400

    Vision and then translate

--- imports/api/cards.js
+++ imports/api/cards.js
@@ -3,24 +3,29 @@ import { Meteor } from 'meteor/meteor';
 import vision from '@google-cloud/vision';
 import Translate from '@google-cloud/translate';
 
-const visionClient = new vision.ImageAnnotatorClient({ projectId: 'plated-bee-197701', keyFilename: Meteor.settings.googleServiceAccountPath });
+const visionClient = new vision.ImageAnnotatorClient({
+    projectId: Meteor.settings.googleCloudProjectName,
+    keyFilename: Meteor.settings.googleServiceAccountPath
+});
+const translateClient = new Translate({
+    projectId: Meteor.settings.googleCloudProjectName,
+    keyFilename: Meteor.settings.googleServiceAccountPath
+});
 
 
 const Cards = new Mongo.Collection('cards');
 
 Meteor.methods({
     async addCard({ imageData }) {
-        const request = {
-            image: { content: imageData }
-        };
-
-        const results = await visionClient.textDetection(request)
+        const results = await visionClient.textDetection({ image: { content: imageData } })
             .then(response => response)
             .catch(err => err);
+        const { text } = results[0].fullTextAnnotation;
+        const translation = await translateClient.translate(text, 'en')
+            .then(response => response[0])
+            .catch(err => err);
 
-        const parsedText = results[0].fullTextAnnotation.text;
-
-        return parsedText;
+        return Cards.insert({ text, translation });
     }
 });
 
